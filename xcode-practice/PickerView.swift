178969c145c8670627b722397e7322ee35a67252
Jiwoo Hong <jiwooh@postech.ac.kr>
2024-08-08 18:39:39 +0900

    add PickerView

--- xcode-practice/PickerView.swift
+++ xcode-practice/PickerView.swift
@@ -8,33 +8,73 @@
 import SwiftUI
 
 struct PickerView: View {
-  @State private var sel = 0
+  enum things: String, CaseIterable {
+    case Apple = "Apple"
+    case Bookmark = "Bookmark"
+    case Carrot = "Carrot"
+    case Door = "Door"
+  }
+  @State private var sel: things = things.Apple
+  
+  func getImage(index: things) -> String {
+    switch index {
+    case .Apple:
+      return "apple.logo"
+    case .Bookmark:
+      return "bookmark.fill"
+    case .Carrot:
+      return "carrot.fill"
+    case .Door:
+      return "door.left.hand.closed"
+    case _: return ""
+    }
+  }
   
   var body: some View {
-    VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/) {
-      Text("Selected value: \(sel)")
+    VStack(alignment: .center) {
+      Image(systemName: getImage(index: sel))
+        .frame(height: 50)
+        .font(.system(size: 40))
+        .foregroundColor(Color.init(hex: 0x5fcc5f))
+        .padding(.bottom, 10)
+      
+      Text("Selected index: \(sel.rawValue)")
+        .font(.system(size: 12))
       Picker (
         selection: $sel,
         label: Text("Picker"),
         content: {
-          Text("Apple").tag(0)
-          Text("Banana").tag(1)
-          Text("Candy").tag(2)
-          Text("Donut").tag(3)
+          ForEach(things.allCases, id: \.self) { item in
+              Text(item.rawValue).tag(item)
+          }
         }
       )
+      .accentColor(Color.init(hex: 0x5fcc5f))
       Picker (
         selection: $sel,
         label: Text("Picker"),
         content: {
-          Text("Apple").tag(0)
-          Text("Banana").tag(1)
-          Text("Candy").tag(2)
-          Text("Donut").tag(3)
+          ForEach(things.allCases, id: \.self) { item in
+              Text(item.rawValue).tag(item)
+          }
         }
       )
       .pickerStyle(SegmentedPickerStyle())
       .padding(.horizontal, 30)
+      Divider().frame(height: 10).opacity(0)
+      
+      ScrollView {
+        ForEach(1...8, id: \.self) { itemIndex in
+          SmallCard(
+            icon: getImage(index: sel),
+            title: "\(sel.rawValue) \(Character(UnicodeScalar("A".unicodeScalars.first!.value + UInt32(itemIndex) - 1)!))",
+            content: "item",
+            bgColor: Color.init(hex: 0x5fcc5f)
+          )
+          .listRowSeparator(.hidden)
+        }
+        .padding(.horizontal, 30)
+      }
     }
     .navigationBarTitle("PickerView", displayMode: .large)
   }
